cf00eaa1b5a5f6d31e2d4bd48dc1860e67d3d529
rishabhmanu <rishabhdevmanu@gmail.com>
2023-05-27 14:58:18 +0530

    added LRU eviction policy logic and minor bug fixes

--- cache.py
+++ cache.py
@@ -35,13 +35,31 @@ class _cache:
         print(self.cache_map)
     
     def element_eviction(self, eviction_policy):
-        pass
+        # Evict the element basis eviction policy
+        temp_node = Node()
+        temp_node.value = (None, None)
+        if eviction_policy == "LRU":
+            if self.cache_list.head:
+                temp_node = self.cache_list.head
+                # removing node from linked list and assigning head to the next node
+                self.cache_list.head = (self.cache_list.head).next
+                if (self.cache_list.head):
+                    (self.cache_list.head).prev = None
 
+        # fetching the key from the temp node
+        key_to_be_removed = temp_node.value[0]
+        if key_to_be_removed:
+            self.cache_map.pop(key_to_be_removed)
+            print("key " + str(key_to_be_removed) + " removed from the cache as per eviction policy")
+        else:
+            print("Nothing to be removed")        
+
+    
     def insert_element_in_cache(self, key, value):
-        new_node = Node(value)
+        new_node = Node((key,value))
         # print(new_node.value)
-        
-        if self.cache_list.head.value == None:
+
+        if (self.cache_list.head == None) or (self.cache_list.head.value == None):
             self.cache_list.head = new_node
             self.cache_list.tail = new_node
         else:
@@ -62,6 +80,7 @@ class _cache:
             self.element_eviction(self.eviction_policy)
         print("inserting initiated for "+ key + " into cache")   
         self.insert_element_in_cache(key, value)
+        print("inserted key "+ key + " into cache")
 
     def get(self, key):
         if self.cache_map.get(key):
@@ -73,12 +92,6 @@ class _cache:
 
                 if candidate_node == self.cache_list.head:
                     self.cache_list.head = candidate_node.next
-                    
-                    # self.cache_list.tail.next = candidate_node
-                    # candidate_node.prev = self.cache_list.tail
-                    # candidate_node.next = None
-
-                    # self.cache_list.tail = candidate_node
                 
                 elif candidate_node.prev:
                     (candidate_node.prev).next = candidate_node.next
@@ -92,32 +105,29 @@ class _cache:
                 
                 self.cache_map[key] = self.cache_list.tail
             
-            return candidate_node.value
+            return candidate_node.value[1]
         
         return None
 
 
 if __name__ == "__main__":
-   demo_cache = _cache(5, "LRU")
+   demo_cache = _cache(1, "LRU")
    demo_cache.put("a", 1)
-#    demo_cache.put("b", 5)
-#    demo_cache.put("c", 9)
-   demo_cache.print_ll()
-   demo_cache.print_map()
-   
-#    print(demo_cache.get("b"))
-#    demo_cache.print_ll()
-#    demo_cache.print_map()
-
-   print(demo_cache.get("a"))
-   demo_cache.print_ll()
-   demo_cache.print_map()
-
-   print(demo_cache.get("a"))
    demo_cache.put("b", 5)
+   demo_cache.put("c", 9)
+   demo_cache.put("d", 15)
+   demo_cache.put("e", 23)
    demo_cache.print_ll()
    demo_cache.print_map()
 
    print(demo_cache.get("a"))
+   demo_cache.print_ll()
+   demo_cache.print_map()
+
+   print(demo_cache.get("d"))
+   demo_cache.print_ll()
+   demo_cache.print_map()
+
+   demo_cache.put("f", 55)
    demo_cache.print_ll()
    demo_cache.print_map()
